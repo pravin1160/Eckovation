016ae08180ab5f13b36ad211d1cc5a4cd438d651
pravin1160 <impravin4u@gmail.com>
2022-09-05 23:43:25 +0530

    java first program

---
+++ JavaClass.java
@@ -0,0 +1,10 @@
+public class JavaClass {
+    int x = 5;
+
+    public static void main(String[] args) {
+        JavaClass myObj1 = new JavaClass();  // Object 1
+        JavaClass myObj2 = new JavaClass();  // Object 2
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+      }
+}
